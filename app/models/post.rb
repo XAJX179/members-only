46d60f8cec01bd45f86b0d6b82f6c27124038a3e
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-11-09 22:18:29 +0530

    feat(User,Post): add minimum length for attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title, presence: true, length: { minimum: 4 }
+  validates :body, presence: true, length: { minimum: 20 }
 end
