2fe5fe37f6b82ee0c1f841134729b33a6bc46690
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-11-10 20:20:58 +0530

    feat(Post): add Post#destroy and permite username parameter on sign up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,33 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [ :new, :create, :destroy ]
+
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.create(post_params)
+    if @post.save!
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
+    @posts = Post.all
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def post_params
+    params.expect(post: [ :title, :body ])
   end
 end
