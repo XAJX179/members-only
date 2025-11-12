43f6c7ce737fae1645483a3dd76d6c18b4f598bb
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-11-12 16:53:29 +0530

    build(production): fix assets not found

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,10 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.relative_url_root = "/members-only"
+  config.assets.prefix = "/members-only/assets"
+  config.public_file_server.enabled = true
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
